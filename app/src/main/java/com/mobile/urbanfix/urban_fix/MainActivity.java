c362df74127f569a2299e809529be0d5c8bd7ce6
hideki <andre.hkiw@gmail.com>
2017-10-29 09:54:55 -0200

    Inserção de novas activities Não está tudo funcionando

--- app/src/main/java/com/mobile/urbanfix/urban_fix/MainActivity.java
+++ app/src/main/java/com/mobile/urbanfix/urban_fix/MainActivity.java
@@ -1,22 +1,35 @@
 package com.mobile.urbanfix.urban_fix;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseDatabase firebase;
-    private FirebaseAuth firebaseAuth;
+    DrawerLayout activityMainDrawerLayout;
+    ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        activityMainDrawerLayout = (DrawerLayout) findViewById(R.id.activityMainDrawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, activityMainDrawerLayout, R.string.open, R.string.close);
 
+        activityMainDrawerLayout.addDrawerListener(mToggle);
+
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
     }
 }
